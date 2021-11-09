b161c1c21b78ded7492559fa579cff00ed6cdba5
maxkenkomoda <tango12speedcell@gmail.com>
2021-11-09 14:24:47 +0900

    create post endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, Query, Response } from '@nestjs/common'
+import { CreatUserDto } from './dto/create-user.dto'
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  create(
+    @Body('username') userName: CreatUserDto,
+    @Query('id') id: string,
+    @Response() res,
+  ) {
+    try {
+      if (!userName || !id) throw new Error('no userName or No user id')
+      res.status(200)
+      res.send({ id, userName })
+      return { id, userName }
+    } catch (e) {
+      res.status(400)
+      console.log(e)
+    }
+  }
+}
